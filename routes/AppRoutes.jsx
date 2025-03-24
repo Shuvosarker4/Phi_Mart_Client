00b4a4d8790219383bb43f188784c977c7edf158
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-03-24 15:19:50 +0600

    first commit

---
+++ routes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import About from "../pages/About";
+
+const AppRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default AppRoutes;
