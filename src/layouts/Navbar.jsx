047b1636131735a178edb79399566bf407dab170
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-03-27 18:47:20 +0600

    Shop page added by sorting,filtering and searchinf

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 const Navbar = () => {
   return (
     <div className="navbar bg-base-100 shadow-sm">
@@ -39,7 +41,7 @@ const Navbar = () => {
               </ul>
             </li>
             <li>
-              <a>Item 3</a>
+              <Link to="/shop">Shop</Link>
             </li>
           </ul>
         </div>
@@ -64,7 +66,7 @@ const Navbar = () => {
             </details>
           </li>
           <li>
-            <a>Item 3</a>
+            <Link to="/shop">Shop</Link>
           </li>
         </ul>
       </div>
